3591dd47526b57c6c80a866847689f5a5f595e05
thiba93 <thibaultsene4@gmail.com>
2026-03-13 09:43:47 +0100

    init

---
+++ fruit-app/fruit-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fruit-app
+//
+//  Created by COURS on 13/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fruit-app/fruit-app/fruit_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruit_appApp.swift
+//  fruit-app
+//
+//  Created by COURS on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct fruit_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
